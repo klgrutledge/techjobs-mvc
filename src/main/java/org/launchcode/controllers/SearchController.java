b2c50f983f1697b21d62a68c37a1a3150dc896bc
Krysta Gasser Rutledge <klgrutledge@gmail.com>
2017-06-05 18:45:44 -0500

    Completed techjobs-mvc (URL change reviewed by Jay)

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ public class SearchController {
         return "search";
     }
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "results")
+    @RequestMapping(value = "", method = RequestMethod.POST)
     public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         if (searchType.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
